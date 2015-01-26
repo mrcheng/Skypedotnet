487a61b49d56ea22565eaff1ae70341547b58c02
benedict <re_b_e_l@mail.ru>
2015-01-26 22:28:02 +0300

    added hosts and app constants

---
+++ src/SkypeDotnet/SkypeDotnet/Constants.cs
@@ -0,0 +1,27 @@
+﻿namespace SkypeDotnet
+{
+    public class Constants
+    {
+        public const int SkypewebMaxMsgRetry = 2;
+        
+        public const string SkypewebPluginId = "prpl-skypeweb";
+        
+        public const string SkypewebPluginVersion = "0.1";
+        
+        public const string SkypewebLockandkeyAppid = "msmsgs@msnmsgr.com";
+        
+        public const string SkypewebLockandkeySecret = "Q1P7W2E4J9R8U3S5";
+        
+        public const string SkypewebContactsHost = "api.skype.com";
+        
+        public const string SkypewebMessagesHost = "client-s.gateway.messenger.live.com";
+        
+        public const string SkypewebLoginHost = "login.skype.com";
+        
+        public const string SkypewebVideomailHost = "vm.skype.com";
+        
+        public const string SkypewebClientinfoName = "swx-skype.com";
+        
+        public const string SkypewebClientinfoVersion = "908/1.0.0.30";
+    }
+}
